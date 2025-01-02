d80a888d2c0cda419fe2c9713c2d6269e3a38e43
David Bruant <davidbruant@protonmail.com>
2025-01-02 23:15:18 +0100

    reducing

--- affichageDossier.js
+++ affichageDossier.js
@@ -1,15 +1,11 @@
-//@ts-check
-
-/** @import {DossierPhase} from './API_Pitchou.d.ts'*/
+/** @import {DossierPhase} from './API_Pitchou.d.ts' */
   
-/**
- *
- * @param {any} x
- * @returns {any}
- */
-export function id(x) {
+
+/** */
+function id(x) {
     return x
 }
+
 
 /** @type {Set<DossierPhase>} */
 export const phases = new Set([
